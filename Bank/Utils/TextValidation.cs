7dfb23a7ebd8fbef6857922e978cfac087f67537
TPNYC\abigail.yashiro <abigail.yashiro@transperfect.com>
2023-07-06 18:30:37 -0400

    changed method bodies to lambda expressions and added IsNullOrEmpty check

--- Bank/Utils/TextValidation.cs
+++ Bank/Utils/TextValidation.cs
@@ -2,13 +2,8 @@ namespace SimpleBankConsoleApp.Utils;
 
 public static class TextValidation
 {
-    public static bool MenuSelection(string input)
-    {
-        return (int.TryParse(input, out var num) && num is >= 1 and <= 6);
-    }
+    public static bool MenuSelection(string? input) => !string.IsNullOrEmpty(input) && int.TryParse(input, out var num) && num is >= 1 and <= 6;
 
-    public static bool Name(string input)
-    {
-        return (input.All(char.IsLetter));
-    }
+    public static bool Name(string? input) => !string.IsNullOrEmpty(input) && input.All(char.IsLetter);
+    
 }
