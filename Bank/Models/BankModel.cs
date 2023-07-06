326db1c1554cd208039188f5e99f1e0f6c122b47
TPNYC\abigail.yashiro <abigail.yashiro@transperfect.com>
2023-07-06 15:26:05 -0400

    Added method to get Account value from Accounts dictionary

--- Bank/Models/BankModel.cs
+++ Bank/Models/BankModel.cs
@@ -4,5 +4,7 @@ namespace SimpleBankConsoleApp.Models;
 
 public class BankModel
 {
-    public Dictionary<Guid, AccountController> Accounts { get; init; }
+    public Dictionary<Guid, AccountController> Accounts { get; } = new();
+    
+    public AccountController this[Guid key] => Accounts[key];
 }
