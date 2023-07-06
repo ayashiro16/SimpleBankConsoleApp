79e7f765645e98105f4f8e3f3c48ad216037c868
TPNYC\abigail.yashiro <abigail.yashiro@transperfect.com>
2023-07-06 15:32:17 -0400

    Removed unused method MoneyAmount, removed null checks on input

--- Bank/Utils/TextValidation.cs
+++ Bank/Utils/TextValidation.cs
@@ -2,18 +2,13 @@ namespace SimpleBankConsoleApp.Utils;
 
 public static class TextValidation
 {
-    public static bool MenuSelection(string? input)
+    public static bool MenuSelection(string input)
     {
-        return (input is not null && Int32.TryParse(input, out int num) && num is >= 1 and <= 6);
+        return (int.TryParse(input, out var num) && num is >= 1 and <= 6);
     }
 
-    public static bool Name(string? input)
+    public static bool Name(string input)
     {
-        return (input is not null && input.All(Char.IsLetter));
-    }
-
-    public static bool MoneyAmount(string? input)
-    {
-        return input is not null && Decimal.TryParse(input, out Decimal amount) && amount > 0;
+        return (input.All(char.IsLetter));
     }
 }
