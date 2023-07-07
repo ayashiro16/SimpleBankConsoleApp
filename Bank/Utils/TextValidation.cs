93a98003137b75cd28a10a4f4948b46076de6242
TPNYC\abigail.yashiro <abigail.yashiro@transperfect.com>
2023-07-07 15:17:59 -0400

    removed unused class

--- Bank/Utils/TextValidation.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleBankConsoleApp.Utils;
-
-public static class TextValidation
-{
-    public static bool MenuSelection(string? input) => !string.IsNullOrEmpty(input) && int.TryParse(input, out var num) && num is >= 1 and <= 6;
-
-    public static bool Name(string? input) => !string.IsNullOrEmpty(input) && input.All(char.IsLetter);
-    
-}
